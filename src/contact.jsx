01e8405de2ced5be6dc993497592db89d7a0e5a8
suman31122000 <sumanbando31122000@gmail.com>
2024-04-12 11:26:02 +0530

    changes

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
 
 
 function Contact(){
+    const [emaildata,setemail]=useState('');
+    const [formData, setFormData] = useState({
+        name: '',
+        email:'',
+        phoneNumber: '',
+        message: ''
+    });
+
+    const handlechange=(e)=>{
+        e.preventDefault();
+        const {name,value}=e.target;
+        setFormData(prevstate=>({...prevstate,
+        [name]:value}));
+    };
+    const email=(e)=>{
+        e.preventDefault();
+        setemail(e.target.value);
+       
+    }
+    const submit=(e)=>{
+        e.preventDefault();
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "sumanbando31122000@gmail.com",
+            Password : "042CC135759F1C32A2C2E67D805B0041EF5D",
+            To : 'official31122000@gmail.com',
+            From : 'sumanbando31122000@gmail.com',
+            Subject : "This is the subject",
+            Body : JSON.stringify(formData),
+        }).then(
+          message => alert(message)
+        ),
+        (error)=>{
+            console.error('error sending email',error);
+        };
+    }
     return(
-        <div className="grid grid-cols-2 gap-4 h-[100vh] bg-gray-900 ">
+        <div id="contact" className="grid grid-cols-2 gap-4 h-[100vh] bg-gray-900 ">
         <div className="flex flex-col justify-center text-white">
             <div className="flex flex-col justify-center items-center my-5">
                 <span>Address</span>
@@ -11,7 +48,7 @@ function Contact(){
             </div >
             <div className="flex flex-col justify-center items-center my-5">
                 <span>Lets Talk</span>
-                <span>+91 7428789074</span>
+                <span >+91 7428789074</span>
             </div>
             <div className="flex flex-col justify-center items-center my-5">
                 <span>Email</span>
@@ -20,22 +57,22 @@ function Contact(){
         </div>
         <div className="flex flex-col justify-center items-center">
             <span className="text-[5vh] my-10 text-white">CONNECT ME</span>
-            <form className="flex flex-col text-white border-4 rounded-lg">
+            <form onSubmit={submit}  className="flex flex-col text-white border-4 rounded-lg">
                 <div className="flex flex-col  justify-center items-center h-[15vh] w-[70vh]">
                     <span className="my-1">TELL ME YOUR NAME</span>
-                    <input type="text" placeholder="Full Name"  className="h-[5vh] w-[60vh] my-4 border-none bg-slate-400 "></input>
+                    <input type="text" name="name" onChange={handlechange} placeholder="Full Name"  className="h-[5vh] w-[60vh] my-4 border-none bg-slate-400 "></input>
                 </div>
                 <div className="flex flex-col  justify-center items-center">
                 <span>ENTER YOUR EMAIL</span>
-                <input type="email" placeholder="Eg exampe@gmail.com "  className="h-[5vh] w-[60vh] my-4 bg-slate-400"></input>
+                <input onChange={email} name="email" type="email" placeholder="Eg exampe@gmail.com "  className="h-[5vh] w-[60vh] my-4 bg-slate-400"></input>
                 </div>
                 <div className="flex flex-col  justify-center items-center">
                 <span>ENTER YOUR PHONE NUMBER</span>
-                <input type="text" placeholder="Eg +184515444587" className="h-[5vh] w-[60vh] my-4 bg-slate-400"></input>
+                <input type="text" name="phonenumber" onChange={handlechange} placeholder="Eg +184515444587" className="h-[5vh] w-[60vh] my-4 bg-slate-400"></input>
                 </div>
                 <div className="flex flex-col  justify-center items-center">
                 <span>MESSAGE</span>
-                <input type="text" placeholder="Write Us a message" className="h-[20vh] w-[60vh] my-4 bg-slate-400"></input>
+                <input type="text" name="message" onChange={handlechange} placeholder="Write Us a message" className="h-[20vh] w-[60vh] my-4 bg-slate-400"></input>
                 </div>
                 <button className=" mx-[190px] my-2  w-32 rounded-md bg-green-400 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">SEND</button>
             </form>
--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,26 @@
+
 const Home=()=>{
+    const handleskill=()=>{
+        const skillsElement = document.getElementById('skills');
+        skillsElement.scrollIntoView({ behavior: 'smooth' });
+    }
+    const handleproject=()=>{
+        const skillsElement = document.getElementById('projects');
+        skillsElement.scrollIntoView({ behavior: 'smooth' });
+    }
+    const handlecontact=()=>{
+        const skillsElement = document.getElementById('contact');
+        skillsElement.scrollIntoView({ behavior: 'smooth' });
+    }
+   
     return(
         <div className="grid sm:grid-cols-3 bg-slate-950 text-white h-10">
-            <div className="text-3xl">PORTFOLIO</div>
+            <div className="text-3xl font-semibold">PORTFOLIO</div>
             <div className="flex gap-10 text-xl">
-                <button className="mx-2 mt-0 h-9 w-32 rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">Skills</button>
-                <button className="mx-2 mt-0 h-9 w-32 rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">Project</button>
-                <button className="mx-2 mt-0 h-9 w-32 rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">Contact</button>
-            </div>
+                <button onClick={handleskill} className="mx-2 mt-0 h-9 w-32 rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">Skills</button>
+                <button onClick={handleproject} className="mx-2 mt-0 h-9 w-32 rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">Project</button>
+                <button onClick={handlecontact} className="mx-2 mt-0 h-9 w-32 rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-800">Contact</button>
+            </div >
             <button className="mx-8 flex items-center justify-end px-8 flex-wrap ">mode</button>
         </div>
     )
