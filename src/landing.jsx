6d6955beddaa748db47e834a687df93eab22d23a
suman31122000 <sumanbando31122000@gmail.com>
2024-04-15 11:41:51 +0530

    update

--- src/landing.jsx
+++ src/landing.jsx
@@ -21,7 +21,7 @@ const Landing = () => {
             <img src="bgimage.jpg" alt="Background" className="w-full h-full object-cover absolute inset-0 z-0" />
             <div className="absolute inset-0 flex flex-row text-white font-semibold">
                 <div className="w-8/12 flex flex-col justify-center mr-8">
-                    <span className="text-3xl md:text-5xl italic text-end">I Am<span className="text-slate-700"> Suman Bando </span></span>
+                    <span className="text-2xl md:text-5xl italic text-end">I Am<span className="text-slate-700 sm:text-6xl"> Suman Bando </span></span>
                     <span className="text-3xl md:text-5xl italic text-end">{developerType}</span>
                     <button className="mt-8 md:mt-12 h-9 w-32 ml-[85%] rounded-md px-8 py-2 text-sm font-semibold text-white shadow-lg shadow-gray-500/50 hover:bg-gray-300 hidden sm:flex z-10">
                         <a href="https://docs.google.com/document/d/1Iua_W5iVfrPA5D8CC4fw1PoLU_gEkNVJ/edit?usp=drive_link&ouid=110023528186454540801&rtpof=true&sd=true" className="block w-full h-full">
--- src/project.jsx
+++ src/project.jsx
@@ -1,41 +1,32 @@
 const Project = () => {
-  return (
+    const projects = [
+      { imageUrl: "EC_Retail_ECommerce_750.jpg", link: "https://suman31122000.github.io/eccomercesite/", title: "Eccomerce" },
+      { imageUrl: "blog2.jpg", link: "https://suman31122000.github.io/pathfinder/", title: "Pathfinder" },
+      { imageUrl: "image1.jpg", link: "https://evokegym.netlify.app", title: "Evogym" },
+      { imageUrl: "port3.jpg", link: "https://quizzverse.netlify.app/", title: "Quizverse" },
+      { imageUrl: "port2.jpg", link: "https://main--sage-cannoli-0c193c.netlify.app/", title: "Todoapp" },
+    ];
+  
+    return (
       <div id="projects" className="flex flex-col bg-slate-900">
-          <div className="flex flex-col justify-center items-center text-white text-[4vh] my-10">
-              <span className="text-[7vh]">MY PROJECT</span>
-              <span className="text-[4vh]">Here is some of my work that I've done in various programming languages</span>
-          </div>
-          <div className="grid gap-8 sm:grid-cols-2 sm:mx-8">
-              <div className="relative h-[35vh] sm:w-[45vh] sm:mx-80">
-                  <img src="EC_Retail_ECommerce_750.jpg" className="w-full h-full object-cover" alt="Eccomerce" />
-                  <div className="absolute inset-0 bg-slate-200 opacity-0 hover:opacity-100 transition-opacity flex flex-col justify-center items-center">
-                      <div className="text-[6vh]">Eccomerce</div>
-                      <a href="https://suman31122000.github.io/eccomercesite/">Visit Me</a>
-                  </div>
+        <div className="flex flex-col justify-center items-center text-white text-[4vh] my-10">
+          <span className="text-[7vh]">MY PROJECT</span>
+          <span className="text-[4vh]">Here is some of my work that I've done in various programming languages</span>
+        </div>
+        <div className="grid gap-4 sm:grid-cols-12 ">
+          {projects.map((project, index) => (
+            <div key={index} className="relative sm:h-[35vh] sm:w-[55vh] col-span-4 sm:mx-20 ">
+              <img src={project.imageUrl} className="w-full h-[35vh] sm:h-full object-cover" alt={project.title} />
+              <div className="absolute inset-0 bg-slate-200 opacity-0 hover:opacity-100 transition-opacity flex flex-col justify-center items-center">
+                <div className="text-[6vh]">{project.title}</div>
+                <a href={project.link}>Visit Me</a>
               </div>
-              <div className="relative h-[35vh] sm:w-[45vh]">
-                  <img src="blog2.jpg" className="w-full h-full object-cover" alt="Pathfinder" />
-                  <div className="absolute inset-0 bg-slate-200 opacity-0 hover:opacity-100 transition-opacity flex flex-col justify-center items-center">
-                      <div className="text-[6vh]">Pathfinder</div>
-                      <a href="https://suman31122000.github.io/pathfinder/">Visit Me</a>
-                  </div>
-              </div>
-              <div className="relative h-[35vh] sm:w-[45vh] sm:mx-80">
-                  <img src="port2.jpg" className="w-full h-full object-cover" alt="Todoapp" />
-                  <div className="absolute inset-0 bg-slate-200 opacity-0 hover:opacity-100 transition-opacity flex flex-col justify-center items-center">
-                      <div className="text-[6vh]">Todoapp</div>
-                      <a href="https://main--sage-cannoli-0c193c.netlify.app/">Visit Me</a>
-                  </div>
-              </div>
-              <div className="relative h-[35vh] sm:w-[45vh]">
-                  <img src="port3.jpg" className="w-full h-full object-cover" alt="Quizverse" />
-                  <div className="absolute inset-0 bg-slate-200 opacity-0 hover:opacity-100 transition-opacity flex flex-col justify-center items-center">
-                      <div className="text-[6vh]">Quizverse</div>
-                      <a href="https://quizzverse.netlify.app/">Visit Me</a>
-                  </div>
-              </div>
-          </div>
+            </div>
+          ))}
+        </div>
       </div>
-  )
-}
-export default Project;
+    );
+  }
+  
+  export default Project;
+  
